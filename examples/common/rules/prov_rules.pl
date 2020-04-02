96422f1f42427d8a25346ba38e0cadb91013850f
Timothy McPhillips <tmcphillips@absoluteflow.com>
2020-04-01 20:19:23 -0700

    Add file:// prefix to all IDs of input and output files

--- examples/common/rules/prov_rules.pl
+++ examples/common/rules/prov_rules.pl
@@ -1,11 +1,12 @@
-
+:- import concat_atom/2 from string.
 
 prov_used_entity_references() :-
     json_comma_init(''),
     wt_data_file(_, FilePath, in),
     json_comma(),
     json_start_object(),
-        json_property('@id', FilePath),
+        concat_atom(['file://', FilePath], FileUri),
+        json_property('@id', FileUri),
     json_end(),
     fail
     ;
@@ -15,7 +16,8 @@ prov_used_entities() :-
     wt_data_file(_, FilePath, in),
     comma,
     json_start_object(),
-        json_property('@id', FilePath), comma,
+        concat_atom(['file://', FilePath], FileUri),
+        json_property('@id', FileUri), comma,
         json_property('@type', 'prov:Entity'),
     json_end(),
     fail
@@ -26,7 +28,8 @@ prov_generated_entities() :-
     wt_data_file(_, FilePath, out),
     comma,
     json_start_object(),
-        json_property('@id', FilePath), comma,
+        concat_atom(['file://', FilePath], FileUri),
+        json_property('@id', FileUri), comma,
         json_property('@type', 'prov:Entity'), comma,
         json_property('prov:wasGeneratedBy', 'tale_execution'),
     json_end(),
--- examples/common/rules/provone_rules.pl
+++ examples/common/rules/provone_rules.pl
@@ -1,10 +1,11 @@
-
+:- import concat_atom/2 from string.
 
 provone_used_data() :-
     wt_data_file(_, FilePath, in),
     comma,
     json_start_object(),
-        json_property('@id', FilePath), comma,
+    concat_atom(['file://', FilePath], FileUri),
+    json_property('@id', FileUri), comma,
         json_property('@type', 'provone:Data'),
     json_end(),
     fail
@@ -15,7 +16,8 @@ provone_generated_data() :-
     wt_data_file(_, FilePath, out),
     comma,
     json_start_object(),
-        json_property('@id', FilePath), comma,
+        concat_atom(['file://', FilePath], FileUri),
+        json_property('@id', FileUri), comma,
         json_property('@type', 'provone:Data'), comma,
         json_property('prov:wasGeneratedBy', 'tale_execution'),
     json_end(),
