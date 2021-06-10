2439de79e0072345a5c51394a29c4b7de072ded0
Timothy McPhillips <tmcphillips@absoluteflow.com>
2021-06-09 22:30:13 -0700

    Add report steps to import and export trace from Blazegraph

--- examples/02-blazegraph-examples/01-data-cmd/report.sh
+++ examples/02-blazegraph-examples/01-data-cmd/report.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-
 RUNNER='../common/run_report_script.sh'
 
-bash ${RUNNER} R1 "Convert trace to RDF triples" << END_COMMAND
+
+bash ${RUNNER} STEP1 "Convert trace to RDF triples" << END_STEP
 
     # convert reprozip trace to RDF triples in Turtle format
 	cpr convert -noroot -notimestamps -from reprozip -to triples -src .reprozip-trace -dest trace.ttl
@@ -11,5 +11,31 @@ bash ${RUNNER} R1 "Convert trace to RDF triples" << END_COMMAND
     # print out the Turtle file
     cat trace.ttl
 
-END_COMMAND
+END_STEP
 
+
+bash ${RUNNER} STEP2 "Prepare Blazegraph for the trace" << END_STEP
+
+    # destroy the trace dataset if it exists in the Blazegraph instance
+    blaze destroy --dataset trace --quiet
+
+    # create the trace dataset in Blazegraph
+    blaze create --dataset trace
+
+END_STEP
+
+
+bash ${RUNNER} STEP3 "Load trace into Blazegraph" << END_STEP
+
+    # load the run trace into the trace dataset in Blazegraph
+    blaze import --dataset trace --format ttl --file trace.ttl
+
+END_STEP
+
+
+bash ${RUNNER} STEP4 "Export the entire trace from Blazegraph" << END_STEP
+
+    # export all of the triples from the trace dataset in Blazegraph
+    blaze export --dataset trace --format nt | sort
+
+END_STEP
