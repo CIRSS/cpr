7fac895dade00dbed4378cb02470592a292d9212
Timothy McPhillips <tmcphillips@absoluteflow.com>
2021-06-14 23:35:28 -0700

    Example run target now runs in workspace directory

--- examples/02-blazegraph-examples/03-hello-c/workspace/run.sh
+++ examples/02-blazegraph-examples/03-hello-c/workspace/run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# enter directory containing this script
-cd `dirname $0`
-
 # run the program
 ./bin/hello_c
--- examples/02-blazegraph-examples/04-date-to-file/workspace/run.sh
+++ examples/02-blazegraph-examples/04-date-to-file/workspace/run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# enter directory containing this script
-cd `dirname $0`
-
 date > outputs/date.txt
 cat `pwd`/outputs/date.txt
