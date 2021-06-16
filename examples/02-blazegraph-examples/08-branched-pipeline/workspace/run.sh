a41fec92ff8a6b36e93379e4c57b092091fdb895
Timothy McPhillips <tmcphillips@absoluteflow.com>
2021-06-15 21:38:25 -0700

    Ported 8th example

---
+++ examples/02-blazegraph-examples/08-branched-pipeline/workspace/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cat inputs/i1.txt inputs/i2.txt > temp/t12.txt
+cat inputs/i1.txt inputs/i2.txt inputs/i3.txt > temp/t123.txt
+cat inputs/i4.txt > temp/t4.txt
+cat temp/t12.txt > outputs/o12.txt
+cat temp/t123.txt temp/t4.txt > outputs/o1234.txt
+cat temp/t4.txt > outputs/o4.txt
